32a65a255519cad6a1334a142e835fe340507747
Maxime <maxrofra@gmail.com>
2022-11-11 23:17:14 +0100

    Added player sprite and background

--- source/main.lua
+++ source/main.lua
@@ -1,8 +1,33 @@
+import "CoreLibs/object"
 import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
 
 local gfx <const> = playdate.graphics
 
-function playdate.update() 
-    gfx.clear()
-    gfx.drawText("Hello World", 20, 20)
+local playerSprite = nil
+
+local function initialize()
+    local playerImage = gfx.image.new("images/player")
+    playerSprite = gfx.sprite.new(playerImage)
+    playerSprite:moveTo(200, 120)
+    playerSprite:add()
+
+    local backgroundImage = gfx.image.new("images/background")
+    gfx.sprite.setBackgroundDrawingCallback(
+        function(x, y, width, height)
+            gfx.setClipRect(x, y, width, height)
+            backgroundImage:draw(0, 0)
+            gfx.clearClipRect()
+        end
+    )
+end
+
+initialize()
+
+function playdate.update()
+    -- Main loop for the game
+    --gfx.clear()
+    --gfx.drawText("Hello World", 20, 20)
+    gfx.sprite.update()
 end
